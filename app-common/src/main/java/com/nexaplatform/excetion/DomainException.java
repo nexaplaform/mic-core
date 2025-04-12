4de138e524a9f728d89648f8ae4e0143bab0e329
Jesis Finol <jfinol02@gmail.com>
2025-04-12 20:16:23 +0200

    Generacion de snapshot

--- app-common/src/main/java/com/nexaplatform/excetion/DomainException.java
+++ app-common/src/main/java/com/nexaplatform/excetion/DomainException.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 public class DomainException extends RuntimeException {
 
-    private final String otro;
     private String description;
     private final String code;
     private final List<String> messages;
 
-    public DomainException(String otro, String code, List<String> messages) {
-        this.otro = otro;
+    public DomainException(String code, List<String> messages) {
         this.code = code;
         this.messages = messages;
     }
@@ -30,9 +28,4 @@ public class DomainException extends RuntimeException {
     public List<String> getMessages() {
         return messages;
     }
-
-    public String getOtro() {
-        return otro;
-    }
-
 }
