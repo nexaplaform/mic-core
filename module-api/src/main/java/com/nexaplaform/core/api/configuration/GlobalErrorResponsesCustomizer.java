aeb0b6a26c8b769e66c7a1b20695a59a1b4bfa23
Nexaplafrom <nexaplaform@gmail.com>
2025-06-17 20:47:42 +0200

    Fix de sonar

--- module-api/src/main/java/com/nexaplaform/core/api/configuration/GlobalErrorResponsesCustomizer.java
+++ module-api/src/main/java/com/nexaplaform/core/api/configuration/GlobalErrorResponsesCustomizer.java
@@ -18,19 +18,19 @@ import java.util.Map;
 @ComponentScan
 public class GlobalErrorResponsesCustomizer implements OpenApiCustomizer {
 
-    public static final String NOT_FOUND = "Not Found";
-    public static final String FORBIDDEN = "Forbidden";
-    public static final String BAD_REQUEST = "Bad Request";
-    public static final String CONFLICT = "Conflict";
-    public static final String UNAUTHORIZED = "Unauthorized";
-    public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
-    public static final String CONTENT_TYPE = "application/json";
     public static final String CODE_401 = "401";
     public static final String CODE_500 = "500";
     public static final String CONDE_404 = "404";
     public static final String CODE_403 = "403";
     public static final String CODE_400 = "400";
     public static final String CODE_409 = "409";
+    public static final String CONFLICT = "Conflict";
+    public static final String FORBIDDEN = "Forbidden";
+    public static final String NOT_FOUND = "Not Found";
+    public static final String BAD_REQUEST = "Bad Request";
+    public static final String UNAUTHORIZED = "Unauthorized";
+    public static final String CONTENT_TYPE = "application/json";
+    public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
 
     @Override
     public void customise(OpenAPI openApi) {
@@ -46,7 +46,7 @@ public class GlobalErrorResponsesCustomizer implements OpenApiCustomizer {
             addApiResponseIfNotPresent(apiResponses, CODE_500, INTERNAL_SERVER_ERROR);
 
             switch (httpMethod) {
-                case GET:
+                case GET, DELETE:
                     addApiResponseIfNotPresent(apiResponses, CONDE_404, NOT_FOUND);
                     addApiResponseIfNotPresent(apiResponses, CODE_403, FORBIDDEN);
                     break;
@@ -55,13 +55,8 @@ public class GlobalErrorResponsesCustomizer implements OpenApiCustomizer {
                     addApiResponseIfNotPresent(apiResponses, CODE_409, CONFLICT);
                     addApiResponseIfNotPresent(apiResponses, CODE_403, FORBIDDEN);
                     break;
-                case PUT:
-                case PATCH:
+                case PUT, PATCH:
                     addApiResponseIfNotPresent(apiResponses, CODE_400, BAD_REQUEST);
-                    addApiResponseIfNotPresent(apiResponses, CONDE_404, NOT_FOUND);
-                    addApiResponseIfNotPresent(apiResponses, CODE_403, FORBIDDEN);
-                    break;
-                case DELETE:
                     addApiResponseIfNotPresent(apiResponses, CONDE_404, NOT_FOUND);
                     addApiResponseIfNotPresent(apiResponses, CODE_403, FORBIDDEN);
                     break;
